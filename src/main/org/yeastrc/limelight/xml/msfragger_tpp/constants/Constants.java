27d979c8b4178c5283827d648d30fa79f9c51656
Michael Riffle <mriffle@uw.edu>
2018-11-28 16:41:36 -0800

    Include PTM Prophet info.

--- src/main/org/yeastrc/limelight/xml/msfragger_tpp/constants/Constants.java
+++ src/main/org/yeastrc/limelight/xml/msfragger_tpp/constants/Constants.java
@@ -5,9 +5,10 @@ public class Constants {
 	public static final String PROGRAM_NAME_MSFRAGGER = "MSFragger";
 	public static final String PROGRAM_NAME_PEPTIDEPROPHET = "PeptideProphet";
 	public static final String PROGRAM_NAME_INTERPROPHET = "InterProphet";
+	public static final String PROGRAM_NAME_PTMPROPHET = "PTMProphet";
 
 	public static final String CONVERSION_PROGRAM_NAME = "msFraggerTPP2LimelightXML.jar";
 	public static final String CONVERSION_PROGRAM_URI = "https://github.com/yeastrc/limelight-import-msfragger-ptm-prophet";
-	public static final String CONVERSION_PROGRAM_VERSION = "0.01";
+	public static final String CONVERSION_PROGRAM_VERSION = "0.02";
 	
 }
